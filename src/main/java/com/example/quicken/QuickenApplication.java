f8465e49b877fc4b83c61b572fb7da9a039eaa02
Parth Jain <parthgmjain@gmail.com>
2025-12-16 14:15:11 +0100

    Initial commit

---
+++ src/main/java/com/example/quicken/QuickenApplication.java
@@ -0,0 +1,13 @@
+package com.example.quicken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickenApplication.class, args);
+	}
+
+}
